f0b1bd3861da91ae6afb763635a67cbdb3b38810
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-04-19 19:39:53 -0700

    added perceptron

--- Perceptron.py
+++ Perceptron.py
@@ -1,25 +1,40 @@
-class Perceptron(): # implement
-  threshold = 0.0 # fix
-  name = ""
+import numpy as np
 
-  def __init__(self, name, dataset):
-    self.name = name
-    self.dataset = dataset
+class Perceptron:
+    def __init__(self, name, num_inputs):
+        self.name = name
+        self.weights = np.random.rand(num_inputs)
+        self.bias = np.random.rand()
 
-  def get_confidence(self): # implement
+    def predict(self, image):
+        # Flatten the image matrix into a 1D array
+        flattened_image = image.flatten()
+        
+        # Compute the weighted sum
+        weighted_sum = np.dot(flattened_image, self.weights) + self.bias
+        
+        # Apply activation function (Step function)
+        return 1 if weighted_sum > 0 else 0
 
-    # for img in dataset
+    def train(self, images, labels, num_epochs, learning_rate):
+        for epoch in range(num_epochs):
+            for image, label in zip(images, labels):
+                prediction = self.predict(image)
+                error = label - prediction
+                # Update weights and bias
+                self.weights += learning_rate * error * image.flatten()
+                self.bias += learning_rate * error
 
-    return 0.0
+    def get_name(self):
+        return self.name
 
-  def get_name(self):
-    return self.name
-
-  def train(): # implement
-    return
-
-  def __str__(self):
-    return self.name
-  
-  def get_dataset(self):
-    return self.dataset
+    def get_confidence(self, image):
+        # Flatten the image matrix into a 1D array
+        flattened_image = image.flatten()
+        
+        # Compute the weighted sum
+        weighted_sum = np.dot(flattened_image, self.weights) + self.bias
+        
+        # Apply activation function (Step function)
+        confidence = weighted_sum
+        return confidence
