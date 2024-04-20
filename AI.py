e036fbfa0f8242e7e15e40e762795847a43b053d
Samyutha Srinivasan <65869364+samyuthasrinivasan@users.noreply.github.com>
2024-04-19 21:06:15 -0700

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,27 +1,18 @@
-# import cv2
-
-# for i in range(35):
-#     #read image
-#     original_image = cv2.imread(f"spain/bollard/BOLLARD#{i+1}.png")
-#     newImage = cv2.resize(original_image, (100,200))
-#     cv2.imwrite("BOLLARD#{i+1}.png", newImage)
-
 class AI():
 
   def __init__(self):
     self.registeredCountries = {}
 
-  def predict(self): # need to fix, for loop is looping through countries and is calling the method on the inner dictionary with the metas instead of singular metas
+  def predict(self, img): # need to fix, for loop is looping through countries and is calling the method on the inner dictionary with the metas instead of singular metas
     list_of_tuples = []
     for country in self.registeredCountries:
       for dict_meta in self.registeredCountries[country]:
-        list_of_tuples.append((dict_meta, dict_meta.get_confidence()))
+        list_of_tuples.append((dict_meta, dict_meta.get_confidence(img)))
 
-    return list_of_tuples
+    return sorted(list_of_tuples, key=lambda x: x[1])
 
   def add_perceptron(self, country, perceptron):
     self.registeredCountries.update({country: perceptron})
 
   def get_dictionary(self):
     return self.registeredCountries
-
